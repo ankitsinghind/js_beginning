6ef9ebbb7f644246b476a2bae5d6a2a6575db028
Ankit Singh <ankitoct03@gmail.com>
2025-08-10 13:51:48 +0530

    seperated html & js files

---
+++ bulb_control.js
@@ -0,0 +1,6 @@
+function myfunction_off() {
+  document.getElementById("bulb").src = "pic_bulb_off.gif";
+}
+function myfunction_on() {
+  document.getElementById("bulb").src = "pic_bulb_on.gif";
+}
